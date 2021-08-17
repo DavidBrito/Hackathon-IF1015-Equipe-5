7a84badb364f34ab2af5be22f83d23cac77c1ed3
Ewerton de Jesus <ewerton.jesus@fabricadenegocio.com.br>
2021-08-17 16:53:48 -0300

    mudanca botao de input e remocao pasta sem uso

--- mybusfinder/src/containers/Home/index.js
+++ mybusfinder/src/containers/Home/index.js
@@ -10,10 +10,10 @@ import L from 'leaflet';
 // pagina principal
 const Home = () => {
     // Hooks de estado, funcao que altera o estado
-    const [ map, setMap ] = useState();
-    const [ selectedBus, setSelectedBus ] = useState('12113');
-    const [ connection, setConnection ] = useState();
-    const [ markers, setMarkers ] = useState();
+    const [map, setMap] = useState();
+    const [selectedBus, setSelectedBus] = useState('12113');
+    const [connection, setConnection] = useState();
+    const [markers, setMarkers] = useState();
 
     // Executa funcao que renderiza o mapa apenas uma vez qnd entra na Home
     useEffect(() => {
@@ -30,10 +30,10 @@ const Home = () => {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-    
+
         // grupo de layers para agrupar os marcadores inseridos no mapa
         const layerGroup = L.layerGroup().addTo(map);
-        
+
         // altera o estado: map e markers
         setMarkers(layerGroup);
         setMap(map);
@@ -55,13 +55,13 @@ const Home = () => {
             connection.onmessage = (e) => {
                 const serverMessage = JSON.parse(e.data);
                 // ["1111","Empresa_13","1111","2017-12-01 06:56:05.360","1","1","286993","9110756","NULL","NULL","NULL","NULL","NULL"]
-                
+
                 // filtra as mensagens com valores indesejados
                 if (["NULL", "TESTE", "0"].includes(serverMessage[6]) || ["NULL", "TESTE", "0"].includes(serverMessage[7])) return;
 
                 // aplica funcao do plugin latlng.UTM
-                const item = L.utm({x: serverMessage[6], y: serverMessage[7], zone: 25, southHemi: true});
-                
+                const item = L.utm({ x: serverMessage[6], y: serverMessage[7], zone: 25, southHemi: true });
+
                 // recebe coordenada latitude longitude
                 const coord = item.latLng();
 
@@ -102,32 +102,18 @@ const Home = () => {
                 <Grid container>
                     <Grid item xs={4} />
                     <Grid item xs={2}>
-                        <Autocomplete
+                        <TextField
                             value={selectedBus}
-                            onChange={(event, newValue) => {
-                                setSelectedBus(newValue);
-                            }}
-                            options={[
-                                '12113',
-                                '12049',
-                                '12452',
-                                '12111',
-                                '12152'
-                            ]}
-                            renderInput={(params) => 
-                                <TextField 
-                                    {...params} 
-                                    label="Número do ônibus" 
-                                    variant="outlined" 
-                                    style={{marginBottom: '2%'}} 
-                                />
-                            }
+                            onChange={(event) => setSelectedBus(event.target.value)}
+                            label="Número do ônibus"
+                            variant="outlined"
+                            style={{ marginBottom: '2%' }}
                         />
                     </Grid>
                     <Grid item xs={1}>
-                        <Button 
+                        <Button
                             size="large"
-                            variant="contained" 
+                            variant="contained"
                             onClick={handleFindBus}
                         >
                             Consultar
@@ -137,9 +123,9 @@ const Home = () => {
                 </Grid>
 
             </Grid>
-            <Grid item xs={1}/>
+            <Grid item xs={1} />
             <Grid item xs={10}>
-                <div style={{height: '70vh', width: '100%'}} id="busMap" />
+                <div style={{ height: '70vh', width: '100%' }} id="busMap" />
             </Grid>
         </Grid>
     )
